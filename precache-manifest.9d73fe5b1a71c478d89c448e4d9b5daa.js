7ee4da632291c87a962c23e5f56ba80e5e3fafae
IG-Kim <iikim2511@gmail.com>
2020-10-24 23:29:27 -0400

    ,,,

---
+++ precache-manifest.9d73fe5b1a71c478d89c448e4d9b5daa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0adc8d568b5d5e8f2d18bbc69fc48d5",
+    "url": "/codingapple-react-2020-1005-classnote-part2-build/index.html"
+  },
+  {
+    "revision": "6d6e3304de6828a00bb3",
+    "url": "/codingapple-react-2020-1005-classnote-part2-build/static/css/main.294ecc29.chunk.css"
+  },
+  {
+    "revision": "cc4fb8e2498b014cbf77",
+    "url": "/codingapple-react-2020-1005-classnote-part2-build/static/js/2.4cf8ebd9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/codingapple-react-2020-1005-classnote-part2-build/static/js/2.4cf8ebd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d6e3304de6828a00bb3",
+    "url": "/codingapple-react-2020-1005-classnote-part2-build/static/js/main.56f4f8b2.chunk.js"
+  },
+  {
+    "revision": "0b0846427f27db1fb6a2",
+    "url": "/codingapple-react-2020-1005-classnote-part2-build/static/js/runtime-main.9509f90d.js"
+  }
+]);
